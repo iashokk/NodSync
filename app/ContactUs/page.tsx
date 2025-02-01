bc946a28163ce386d545d378b4773f038d9ce033
iashokk <ashokkumaravel02@gmail.com>
2025-02-02 02:35:20 +0530

    Header update

---
+++ app/ContactUs/page.tsx
@@ -0,0 +1,27 @@
+export const metadata = {
+    title: "Contact Us - NodSync",
+    description: "Contact Us",
+  };
+  
+  import Link from "next/link";
+  
+  export default function ContactUs() {
+    return (
+      <section>
+        <div className="mx-auto max-w-6xl px-4 sm:px-6">
+          <div className="py-12 md:py-20">
+            {/* Section header */}
+            <div className="pb-12 text-center">
+            <h1
+              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
+              data-aos="fade-up"
+            >
+              Contact
+            </h1>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+  
