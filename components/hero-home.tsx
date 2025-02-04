b75bf2a21d49eba3f8b707cdc8825c42a8ee7da3
iashokk <ashokkumaravel02@gmail.com>
2025-02-04 22:57:00 +0530

    commit work

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -6,7 +6,7 @@ export default function HeroHome() {
         {/* Hero content */}
         <div className="py-12 md:py-20">
           {/* Section header */}
-          <div className="pb-12 text-center md:pb-20">
+          <div className="pb-12 text-center md:pb-10">
             <h1
               className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
               data-aos="fade-up"
--- components/workflows.tsx
+++ components/workflows.tsx
@@ -10,7 +10,21 @@ export default function Workflows() {
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
         <div className="pb-12 md:pb-20">
           {/* Section header */}
-          
+          <div className="mx-auto max-w-3xl pb-12 text-center md:pb-20">
+            <div className="inline-flex items-center gap-3 pb-3 before:h-px before:w-8 before:bg-gradient-to-r before:from-transparent before:to-indigo-200/50 after:h-px after:w-8 after:bg-gradient-to-l after:from-transparent after:to-indigo-200/50">
+              <span className="inline-flex bg-gradient-to-r from-indigo-500 to-indigo-200 bg-clip-text text-transparent">
+              Streamlined Solutions
+              </span>
+            </div>
+            <h2 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-4 font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
+              Map your Project journey
+            </h2>
+            <p className="text-lg text-indigo-200/65">
+            At NodSync, we’re on a mission to empower dreamers to 
+            achieve their academic and career goals with confidence and clarity. NodSync is your friendly
+             guide at every step of the journey.
+            </p>
+          </div>
           {/* Spotlight items */}
           <Spotlight className="group mx-auto grid max-w-sm items-start gap-6 lg:max-w-none lg:grid-cols-3">
             {/* Card 1 */}
