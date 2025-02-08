464b574f06efa8822c8938ffa9484fcd12e2206f
Logesh08 <logeshkrishna08@gmail.com>
2025-02-08 18:58:41 +0530

    NodSync25 -> NodSync

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -32,7 +32,7 @@ export default function Footer() {
               <li>
                 <a
                   className="flex items-center justify-center text-indigo-500 transition hover:text-indigo-400"
-                  href="https://github.com/NodSync25"
+                  href="https://github.com/NodSync"
                   target="_blank"
                   rel="noopener noreferrer"
                   aria-label="GitHub"
