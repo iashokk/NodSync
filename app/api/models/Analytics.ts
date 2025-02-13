71f2aeb30e88318c7e99e316aa9d8a2d2b90429a
Logesh08 <logeshkrishna08@gmail.com>
2025-02-13 13:25:26 +0530

    Removed mongoDb completely

--- app/api/models/Analytics.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const AnalyticsSchema = new mongoose.Schema({
-  page: { type: String, required: true, unique: true },
-  views: { type: Number, default: 0 },
-});
-
-export default mongoose.models.Analytics || mongoose.model('Analytics', AnalyticsSchema);
